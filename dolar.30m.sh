75b0a0e7c94bd6dd6e3cede2aaf033546033b577
Iñaki Abete <inakiabt@users.noreply.github.com>
2019-05-06 15:34:19 -0300

    Create dolar.30m.sh

---
+++ dolar.30m.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get current argentinian Dollar value
+#
+# by Iñaki Abete (inakiabt@gmail.com)
+#
+# Shows current argentinian Dollar value
+# 10 second refresh might be a little too quick. Tweak to your liking.
+
+exit_with_error() {
+  echo "err | color=red";
+  exit 1;
+}
+export NVM_DIR=$HOME/.nvm
+. /usr/local/opt/nvm/nvm.sh
+
+result=`$(nvm which default) $HOME/src/dolar-bitbar-plugin/index.js || exit_with_error`
+
+value=`echo $result | awk '{print $1}' | head -1`
+
+echo "💵"
+echo "---";
+echo "$result";
